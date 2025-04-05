9dfe10c66401db332fc603371cb8481636c2b0c8
Justin Potter <jpott09@gmail.com>
2025-04-05 10:53:22 -0700

    pathing

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // imports
-import { Network } from "js/network.js";
-import { Display } from "js/display.js";
+import { Network } from "./js/network.js";
+import { Display } from "./js/display.js";
 // paths
-const fittings_path = `${location.pathname.split('/').slice(0,2).join('/')}/data/Fittings.json`;
+const base = `/${location.pathname.split('/')[1]}`;
+const fittings_path = `${base}/data/Fittings.json`;
 // elements
 // data
 let display = new Display(document,await Network.jsonRequest(fittings_path));
