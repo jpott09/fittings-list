339e32918bbcfa47ca0aafd13d00fb5e766c0f2b
Justin Potter <jpott09@gmail.com>
2025-04-05 10:51:54 -0700

    pathing

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 import { Network } from "js/network.js";
 import { Display } from "js/display.js";
 // paths
-const fittings_path = "/fittings-list/data/Fittings.json";
+const fittings_path = `${location.pathname.split('/').slice(0,2).join('/')}/data/Fittings.json`;
 // elements
 // data
 let display = new Display(document,await Network.jsonRequest(fittings_path));
